1f60e1f72786925b82318c1818364d50460ca3d7
Antonio Lopez <pezlop202@gmail.com>
2023-02-08 15:57:19 -0800

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    preact(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
+  ],
+});
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between font-["Playfair_Display"]'>
+      <a href='/'>The Gizzverse</a>
+    </nav>
+  );
+};
+
+export default Navbar;
