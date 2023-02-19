0e7f377b541477fb9777b7e07248ceec97e83678
Antonio Lopez <pezlop202@gmail.com>
2023-02-18 20:14:49 -0800

    Add Layout, Hero, and Biography component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,19 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import image from '@astrojs/image';
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     preact(),
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
   ],
 });
